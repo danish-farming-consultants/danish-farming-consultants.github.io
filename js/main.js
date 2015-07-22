98bf44a27296524f47fd4fc2ce8bbeeba9f1e9c5
Michal Kowol <michal.kowol@vimn.com>
2015-07-22 10:03:18 +0200

    NT. Menu.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-/*!
- * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 (function($) {
-    "use strict"; // Start of use strict
+    "use strict";
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
@@ -20,7 +14,7 @@
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
-    })
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -40,9 +34,9 @@
         offset: {
             top: 100
         }
-    })
+    });
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
-})(jQuery); // End of use strict
+})(jQuery);
