4f4dc2240c48ebf1e06b63a5a39bdb753c87af4f
Michal Kowol <michal.kowol@gmail.com>
2015-08-17 17:02:54 +0200

    NT. DB mocks.

---
+++ api/getNews.php
@@ -0,0 +1,32 @@
+[
+  {
+    "id": 1,
+    "createdDate": "2015-08-05",
+    "title": "Prezydent z\u0142o\u017cy\u0142 kwiaty na grobie Lecha i Marii Kaczy\u0144skich na Wawelu",
+    "body": "07.08. Krak\u00f3w (\u0104\u0106\u017b) - Prezydent Andrzej Duda z\u0142o\u017cy\u0142 w pi\u0105tek wieczorem kwiaty na sarkofagu Lecha i Marii Kaczy\u0144skich oraz J\u00f3zefa Pi\u0142sudskiego w krypcie pod Wie\u017c\u0105 Srebrnych Dzwon\u00f3w w Katedrze na Wawelu. Towarzyszy\u0142a mu jedynie ma\u0142\u017conka Agata Duda."
+  },
+  {
+    "id": 4,
+    "createdDate": "2015-08-05",
+    "title": "Pszczo\u0142y na dachu Pa\u0142acu Kultury i Nauki",
+    "body": "07.08. Warszawa (PAP) - Na dachu Pa\u0142acu Kultury i Nauki pojawi\u0142a si\u0119 pasieka z czterema pszczelimi rodzinami. Obok uli znajduje si\u0119 szpaler miododajnych krzew\u00f3w. Dzi\u0119ki miejskiej hodowli pszcz\u00f3\u0142 mo\u017cliwe jest wzmocnienie populacji tego gatunku."
+  },
+  {
+    "id": 2,
+    "createdDate": "2015-08-03",
+    "title": "Co najmniej 35 ofiar zamach\u00f3w w Kabulu",
+    "body": "07.08. Kabul (PAP) - W trzech zamachach w Kabulu zgin\u0119\u0142o w pi\u0105tek co najmniej 35 os\u00f3b. Nad ranem eksplodowa\u0142a ci\u0119\u017car\u00f3wka pu\u0142apka, drugiego ataku dokona\u0142 zamachowiec samob\u00f3jca, okoliczno\u015bci trzeciego zamachu nie s\u0105 jeszcze jasne - poda\u0142y afga\u0144skie w\u0142adze."
+  },
+  {
+    "id": 5,
+    "createdDate": "2015-07-11",
+    "title": "Prezes TVP: naszym celem jest telewizja obywatelska",
+    "body": "07.08. Warszawa (PAP) - Powr\u00f3t do misyjno\u015bci, s\u0142u\u017cby spo\u0142ecze\u0144stwu i narracji publicznej w funkcjonowaniu TVP zapowiedzia\u0142 w pi\u0105tek nowy prezes sp\u00f3\u0142ki Janusz Daszczy\u0144ski - poinformowa\u0142o Centrum Informacji TVP. Zapowiedzia\u0142 r\u00f3wnie\u017c, \u017ce nie b\u0119dzie kolejnego outsourcingu pracownik\u00f3w."
+  },
+  {
+    "id": 3,
+    "createdDate": "2015-04-06",
+    "title": "LE siatkarzy - Polska - Macedonia 2:3",
+    "body": "07.08. Kalisz (PAP) - Polscy siatkarze przegrali w Kaliszu z Macedoni\u0105 2:3 (24:26, 25:22, 26:24, 20:25, 12:15) w dziewi\u0105tym meczu grupy A Ligi Europejskiej. To trzecia pora\u017cka podopiecznych Andrzeja Kowala w rozgrywkach3."
+  }
+]
---
+++ api/getOffers.php
@@ -0,0 +1,23 @@
+[
+  {
+    "id": 1,
+    "name": "TUCZNIKI",
+    "amount": 102,
+    "weightMin": 64,
+    "weightMax": 80
+  },
+  {
+    "id": 2,
+    "name": "TUCZNIKI",
+    "amount": 110,
+    "weightMin": 80,
+    "weightMax": 90
+  },
+  {
+    "id": 3,
+    "name": "TUCZNIKI",
+    "amount": 150,
+    "weightMin": 100,
+    "weightMax": 120
+  }
+]
